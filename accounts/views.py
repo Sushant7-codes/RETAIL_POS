643a14d83e6421233942ece7a5ae8910fe54e5b0
Sushant7-codes <sushantpahari06@gmail.com>
2025-08-28 15:29:19 +0545

    LOGIN AND REGISTER BASIC

---
+++ accounts/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render,redirect
+
+# Create your views here.
+
+def login_view(request):
+    return render(request, 'accounts/login.html')
+
+def register_view(request):
+    return render(request, 'accounts/register.html')
+
+def home_redirect(request):
+    return redirect('/accounts/login/')
